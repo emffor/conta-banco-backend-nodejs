7922e3e24ac57cae40d4ab0991ef0c3d325400eb
Eloan Ferreira <98027423+emffor@users.noreply.github.com>
2022-06-28 17:32:54 -0300

    aula7

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ function verifyIfExistsAccountCPF(request, response, next) {
    return next();
 }
 
+//receber o statement da conta, que armazena o extrato da conta
+//.reduce() -> transforma todas os valores passado em um único valor - no caso o que entrou - o que saiu.
+//operation é o que está sendo adicionado ou removendo dentro do objeto.
+function getBalance(statement) {
+   const balance = statement.reduce((acc, operation) => {
+      if (operation.type === 'credit') {
+         return acc + operation.amount;
+      } else {
+         return acc - operation.amount;
+      }
+   }, 0);
+
+   return balance;
+}
+
 /*
  * cpf = string
  * nome = string
@@ -62,6 +77,7 @@ app.post('/account', (request, response) => {
 //usar dessa forma quando eu quiser que todas as minhas rotas tenha acesso ao middleware
 //app.use(verifyIfExistsAccountCPF);
 
+//método para buscar o extrato bancário
 app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
    //recuperando informações do cliente - request.customers desse middleware
    const { customer } = request;
@@ -69,6 +85,7 @@ app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
    return response.json(customer.statement);
 });
 
+//método para realizar um depósito
 app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
    const { description, amount } = request.body;
 
@@ -83,6 +100,33 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
 
    customer.statement.push(statementOperation);
 
+   return response.status(201).send();
+});
+
+//método para sacar dinheiro - regra de negocio saldo não pode ser menor que 0
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+   const { amount } = request.body;
+   const { customer } = request;
+
+   //onde fica as nossas operações
+   const balance = getBalance(customer.statement);
+
+   //se nao tiver saldo suficiente enviara um erro
+   if (balance < amount) {
+      return response.status(400).json({ error: 'Insufficient Funds!' });
+   }
+
+   //se tiver saldo suficiente
+   const statementOperation = {
+      amount,
+      created_at: new Date(),
+      type: 'debit',
+   };
+
+   //adicionando a operação no extrato
+   customer.statement.push(statementOperation);
+
+   //retornando o extrato
    return response.status(201).send();
 });
 
